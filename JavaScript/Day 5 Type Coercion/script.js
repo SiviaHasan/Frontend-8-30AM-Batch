f45e1a90d3930de7dc6526a187b82e719e15c6fb
SiviaHasan <hasansivia941@gmail.com>
2024-05-29 13:04:02 +0530

    type coercion

---
+++ JavaScript/Day 5 Type Coercion/script.js
@@ -0,0 +1,86 @@
+//String into number
+// const a="14";
+// const b=Number(a);
+// console.log(b,typeof b)
+
+// const a="14";
+// const b=+a;
+// console.log(b,typeof b)
+
+// const c=+prompt('Enter your age');
+// console.log(c)
+// console.log(typeof(c))
+
+
+// Convert strict string into number
+// const b="Hello"
+// const c=+b
+// console.log(c,typeof c)
+
+// Convert  string into bignit
+// const b="20"
+// const c=BigInt(b)
+// console.log(c,typeof c)
+
+//In case of strict string it give error
+// const b="Hello"
+// const c=BigInt(b)
+// console.log(c,typeof c)
+
+
+// Convert number  into bignit
+// const b=15
+// const c=BigInt(b)
+// console.log(c,typeof c)
+
+// Convert number  into String
+// const b=15
+// const c=String(b)
+// console.log(c,typeof c)
+
+// Convert number into boolean
+// const b=0
+// const c=Boolean(b)
+// console.log(c,typeof c)
+
+// Convert boolen  into number
+// const b=true
+// const c=Number(b)
+// console.log(c,typeof c)
+
+// Convert boolen  into BigInt
+// const b=true
+// const c=BigInt(b)
+// console.log(c,typeof c)
+
+// const d=-4
+// const e=Boolean(d)
+// console.log(e,typeof e)
+// // Except 0 it convert all numbers into true 
+// const f=0.9
+// const g=Boolean(d)
+// console.log(g,typeof g)
+
+// Convert BigInt into boolean
+// const b=0n
+// const c=Boolean(b)
+// console.log(c,typeof c)
+
+// Convert String into boolean
+// If string is empty convert into false otherwise true
+// const b=''
+// const c=Boolean(b)
+// console.log(c,typeof c)
+// const b='hi'
+// const c=Boolean(b)
+// console.log(c,typeof c)
+
+//Convert null into number
+// const b=null;
+// const c=Number(b)
+// console.log(c,typeof(c))
+
+//Convert number can not converted into  null 
+// const b=14;
+// const c=Null(b);
+// console.log(c,typeof(c))
