13e2bcbaff5a50bb89ad3194ffdead1194d21a41
SiviaHasan <hasansivia941@gmail.com>
2024-06-04 22:17:40 +0530

    js q

---
+++ JavaScript/Day-8 Hoisting & TDZ/question.js
@@ -0,0 +1,29 @@
+// console.log("start");
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 100;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+console.log("start");
+let a = 10;
+var b = 20;
+const c = 30;
+{
+  let a = 10;
+  console.log(a);
+  const c = 300;
+  console.log(b);
+  b = 200;
+  c = 30;
+  console.log(b);
+}
+console.log(a);
+console.log(b);
+console.log("end");
