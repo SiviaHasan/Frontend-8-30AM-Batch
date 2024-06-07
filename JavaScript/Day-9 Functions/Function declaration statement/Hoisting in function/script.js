441e139f30d09bed24009905f3b01964c91b01db
SiviaHasan <hasansivia941@gmail.com>
2024-06-07 07:39:48 +0530

    function declaration statement

---
+++ JavaScript/Day-9 Functions/Function declaration statement/Hoisting in function/script.js
@@ -0,0 +1,24 @@
+// greet();
+// function greet(){
+//   console.log("Good morning")
+// }
+
+console.log("start")
+let b=1;
+var c=3;
+const d=3;
+greet();
+function greet(){
+  console.log("Good morning")
+  var c=20;
+   b=10;
+   console.log(b)
+   console.log(c)
+   console.log(d)
+
+   return undefined
+}
+console.log(b)
+console.log(c)
+console.log(d)
+console.log("end")
---
+++ JavaScript/Day-9 Functions/Function declaration statement/script.js
@@ -0,0 +1,9 @@
+function akshayKumar(){
+  console.log("Tu samjha nhi tu nhi samjha ");
+  return "Hello"
+}
+
+akshayKumar();
+console.log(akshayKumar)
+console.log(akshayKumar())
+console.log(typeof akshayKumar)
