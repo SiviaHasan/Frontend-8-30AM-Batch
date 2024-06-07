65a6dbbad8f57555566fc692171569ce4453c054
SiviaHasan <hasansivia941@gmail.com>
2024-06-07 07:51:30 +0530

    First class function

---
+++ JavaScript/Day-9 Functions/First class function/scipt.js
@@ -0,0 +1,22 @@
+// console.log(printSum)
+// console.log(printSum(13,12))
+// console.log(typeof printSum)
+
+
+// const printSum=function (a,b){
+//   let sum=a+b;
+//   return sum;
+// }
+// console.log(printSum)
+// console.log(printSum(13,12))
+// console.log(typeof printSum)
+
+
+console.log(printSum)
+// console.log(printSum(13,12))
+console.log(typeof printSum)
+
+var printSum=function (a,b){
+  let sum=a+b;
+  return sum;
+}
